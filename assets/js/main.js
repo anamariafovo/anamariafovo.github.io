3dfefbc063d16d9b8fb78aabe325c5fb59910128
AnamariaFoVo <anamaria_fotache@yahoo.com>
2024-09-11 15:47:21 +0200

    added dark theme function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,27 @@
 /*========================================== DARK/LIGHT THEME ==========================================*/
-/*========================================== DARK/LIGHT THEME ==========================================*/
 const themeButton = document.getElementById('theme-button');
+const lightThemeIcon = document.getElementById('light-theme-button');
+const darkThemeIcon = document.getElementById('dark-theme-button');
 
-if (themeIcon) {
-  const lightThemeIcon = document.getElementById('light-theme-button');
-  const darkThemeIcon = document.getElementById('dark-theme-button');
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
 
-  const selectedTheme = localStorage.getItem('selected-theme');
-  const selectedIcon = localStorage.getItem('selected-theme-icon');
+const getCurrentTheme = () =>
+  document.body.classList.contains('dark-theme') ? 'dark' : 'light';
 
-  const getCurrentTheme = () =>
-    document.body.classList.contains(darkTheme) ? 'dark' : 'light';
-  const getCurrentIcon = () =>
-    themeIcon.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
-
-  if (selectedTheme) {
-    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
-      darkTheme
-    );
-    themeIcon.classList[selectedIcon === 'dark-theme-button' ? 'add' : 'remove'](
-        lightThemeIcon
-    );
+themeButton.addEventListener('click', () => {
+  if (getCurrentTheme() == 'dark') {
+    document.body.classList.remove('dark-theme');
+    darkThemeIcon.style.display = 'none';
+    lightThemeIcon.style.display = 'inline';
+  } else {
+    document.body.classList.add('dark-theme');
+    darkThemeIcon.style.display = 'inline';
+    lightThemeIcon.style.display = 'none';
   }
 
-  themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme);
-    themeIcon.classList.toggle(iconTheme);
-    // Save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme());
-    localStorage.setItem('selected-theme-icon', getCurrentIcon());
-  });
-}
+  localStorage.setItem('selected-theme', getCurrentTheme());
+});
 
 /*========================================== TOGGLE MENU ==========================================*/
 
@@ -41,4 +30,5 @@ if (themeIcon) {
 /*========================================== SHOW PROJECT LIST ==========================================*/
 
 /*========================================== SET DATE DYNAMICALLY ==========================================*/
-
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
