d6bf24e2934f775037c66db51e228d59324df56c
AnamariaFoVo <anamaria_fotache@yahoo.com>
2025-01-19 18:46:40 +0100

    implement presentation navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,14 @@ themeButton.addEventListener('click', () => {
   }
 });
 
+document.onscroll = function() {
+  if (window.innerHeight + window.scrollY > document.body.clientHeight) {
+      document.getElementById('presentation-navbar').style.display='none';
+  } else {
+    document.getElementById('presentation-navbar').style.display='flex';
+  }
+}
+
 /*========================================== TOGGLE MENU ==========================================*/
 
 /*========================================== REMOVE MENU MOBILE ==========================================*/
