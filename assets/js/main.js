e9d94adc6a5c3333b541c10f96dd373fb7dbe028
AnamariaFoVo <anamaria_fotache@yahoo.com>
2025-10-05 15:30:47 +0200

    Fix small JS console error and add new navigation items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,91 +1,97 @@
 /*========================================== DARK/LIGHT THEME ==========================================*/
-const themeButton = document.getElementById('theme-button');
-const lightThemeIcon = document.getElementById('light-theme-button');
-const darkThemeIcon = document.getElementById('dark-theme-button');
+const themeButton = document.getElementById("theme-button");
+const lightThemeIcon = document.getElementById("light-theme-button");
+const darkThemeIcon = document.getElementById("dark-theme-button");
 
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
 const getCurrentTheme = () =>
-  document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+  document.body.classList.contains("dark-theme") ? "dark" : "light";
 
-if (localStorage.getItem('selected-theme') == 'dark') {
-  document.body.classList.add('dark-theme');
-  darkThemeIcon.style.display = 'inline';
-  lightThemeIcon.style.display = 'none';
+if (localStorage.getItem("selected-theme") == "dark") {
+  document.body.classList.add("dark-theme");
+  darkThemeIcon.style.display = "inline";
+  lightThemeIcon.style.display = "none";
 } else {
-  document.body.classList.remove('dark-theme');
-  darkThemeIcon.style.display = 'none';
-  lightThemeIcon.style.display = 'inline';
+  document.body.classList.remove("dark-theme");
+  darkThemeIcon.style.display = "none";
+  lightThemeIcon.style.display = "inline";
 }
 
-themeButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark-theme');
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
 
-  if (getCurrentTheme() == 'dark') {
-    darkThemeIcon.style.display = 'inline';
-    lightThemeIcon.style.display = 'none';
-    localStorage.setItem('selected-theme', 'dark');
+  if (getCurrentTheme() == "dark") {
+    darkThemeIcon.style.display = "inline";
+    lightThemeIcon.style.display = "none";
+    localStorage.setItem("selected-theme", "dark");
   } else {
-    darkThemeIcon.style.display = 'none';
-    lightThemeIcon.style.display = 'inline';
-    localStorage.setItem('selected-theme', 'light');
+    darkThemeIcon.style.display = "none";
+    lightThemeIcon.style.display = "inline";
+    localStorage.setItem("selected-theme", "light");
   }
 });
 
 /*========================================== TOGGLE NAVBAR MENU ==========================================*/
-var navbarElements = document.getElementById('navbarElements');
-var navbarButton = document.getElementById('navbarButton');
+var navbarElements = document.getElementById("navbarElements");
+var navbarButton = document.getElementById("navbarButton");
 
 if (window.innerWidth > 768) {
-  navbarButton.style.display = 'none';
+  navbarButton.style.display = "none";
 } else {
-  navbarButton.style.display = 'block';
+  navbarButton.style.display = "block";
 }
 
-navbarButton.addEventListener('click', (event) => {
+navbarButton.addEventListener("click", (event) => {
   event.preventDefault();
-  navbarElements.classList.toggle('show-navbar-elements');
+  navbarElements.classList.toggle("show-navbar-elements");
 });
 
 if (window.innerWidth <= 768) {
-  var navbarLinks = document.getElementById('navbarLinks').children;
+  var navbarLinks = document.getElementById("navbarLinks").children;
   for (i = 0; i < navbarLinks.length; ++i) {
-    navbarLinks[i].addEventListener('click', () => {
-      navbarElements.classList.remove('show-navbar-elements');
+    navbarLinks[i].addEventListener("click", () => {
+      navbarElements.classList.remove("show-navbar-elements");
     });
   }
 }
 
 /*========================================== HIDE PROJECT MENU ON SCROLL ==========================================*/
-var projectMenu = document.getElementById('projectPresentationNavbar');
+var projectMenu = document.getElementById("projectPresentationNavbar");
 
-document.onscroll = function() {
-  if (window.innerHeight + window.scrollY > document.body.clientHeight) {
-      projectMenu.style.display =' none';
-  } else {
-    projectMenu.style.display = 'flex';
-  }
+if (projectMenu) {
+  document.onscroll = function () {
+    if (window.innerHeight + window.scrollY > document.body.clientHeight) {
+      projectMenu.style.display = " none";
+    } else {
+      projectMenu.style.display = "flex";
+    }
+  };
 }
 
 /*========================================== TOGGLE PROJECT MENU ==========================================*/
-var projectPresentationNavbarButton = document.getElementById('projectPresentationNavbarButton');
+var projectPresentationNavbarButton = document.getElementById(
+  "projectPresentationNavbarButton"
+);
 
-if (window.innerWidth > 768) {
-  projectPresentationNavbarButton.style.display = 'none';
-} else {
-  projectPresentationNavbarButton.style.display = 'block';
+if (projectPresentationNavbarButton) {
+  if (window.innerWidth > 768) {
+    projectPresentationNavbarButton.style.display = "none";
+  } else {
+    projectPresentationNavbarButton.style.display = "block";
+  }
+
+  projectPresentationNavbarButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    projectPresentationNavbarButton.innerText =
+      projectPresentationNavbarButton.innerText === "Open Menu"
+        ? "Close Menu"
+        : "Open Menu";
+    projectMenu.classList.toggle("show-project-presentation-navbar");
+  });
 }
 
-projectPresentationNavbarButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  projectPresentationNavbarButton.innerText = 
-    (projectPresentationNavbarButton.innerText === 'Open Menu')
-    ? 'Close Menu' 
-    : 'Open Menu';
-  projectMenu.classList.toggle('show-project-presentation-navbar');
-});
-
 /*========================================== SET DATE DYNAMICALLY ==========================================*/
-const date = document.getElementById('date');
+const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
